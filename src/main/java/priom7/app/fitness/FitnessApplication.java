fd701c41236f1e2c6b16622d95fb0c18c4aab3c3
priom7 <priom7197@gmail.com>
2024-07-03 19:15:23 +0100

    [WIP] Testing Flow

--- src/main/java/priom7/app/fitness/FitnessApplication.java
+++ src/main/java/priom7/app/fitness/FitnessApplication.java
@@ -2,12 +2,17 @@ package priom7.app.fitness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import test.bar.TestClass;
 
 @SpringBootApplication
 public class FitnessApplication {
 
 	public static void main(String[] args) {
+
+		var testText = new TestClass();
 		SpringApplication.run(FitnessApplication.class, args);
+		System.out.println("Fitness Application started");
+		System.out.println(testText.testMethod());
 	}
 
 }
---
+++ src/main/java/test/bar/TestClass.java
@@ -0,0 +1,11 @@
+package test.bar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestClass {
+
+    public String testMethod() {
+        return "Hello World";
+    }
+}
